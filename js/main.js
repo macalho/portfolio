ecebcfff341a3c994453d8d5f9a1e73b3e8e63e4
Marcelo Carvalho <macalho@gmail.com>
2017-01-23 20:05:22 -0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('body').scrollspy({ target: '#fixed-navbar' });
+});
+
+function openTwitter() {
+	window.open("https://twitter.com/macalho");
+};
+
+function openFreeCodeCamp() {
+	window.open("https://www.freecodecamp.com/macalho");
+};
+
+function openGitHub() {
+	window.open("https://github.com/macalho");
+};
+
+function openLinkedIn() {
+	window.open("https://br.linkedin.com/in/macalho");
+};
